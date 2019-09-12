039e8552353b58aab6329c2e5e76b5e1016e1bd3
Idolphint <litiantianper@163.com>
2019-09-12 13:37:07 +0800

    1 unfinish

---
+++ testfile1.c
@@ -0,0 +1,14 @@
+//常量说明 变量说明 + 返回值函数 + 无返回函数 + 主函数
+
+//常数说明
+const int a1a1=100,b1b1=200;
+const char c1c1='+'
+const char c2='/'
+const char c3='d'
+const char c4='0'
+
+//变量说明
+int ff;
+char gg[0];
+int hh[10], num[2];
+
